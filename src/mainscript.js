47347c423fd929aad6c14fd433ad5304765a680c
William Fauriat <180698994+wfauriat@users.noreply.github.com>
2025-02-09 23:15:31 +0100

    large commit

--- src/mainscript.js
+++ src/mainscript.js
@@ -1,94 +1,133 @@
-// $("#liProfile").on({
-//     click: function(){
-//         $("article:first").css("color","red");
-//         $("article:not(:first-child)").hide()
-//         $("#Profile").show()
 
-//     },
-//     mouseleave: function(){
-//         $("article:first").css("color","black")
-//         $("article:not(:first-child)").show()
-//         $("#Profile").hide()
-//     }
-// })
+$(document).ready(function() {
+    loadOverview();
+    $("#liOverview").css("background-color","white");
+});
 
-// $("#Profile").hide()
-
-$("#liProfile").on({
-    click: function(){
-        $("#Profile").css("display","block");
-        $("#Experience").css("display","none");
-        $("#Education").css("display","none");
-        $("#Expertise").css("display", "none");
-        $("#Tech").css("display", "none");
-        $("#Goals").css("display", "none");
+$("#liOverview").click(
+    function(){
+        loadOverview();
+        $(this).css("background-color","white");
+        $("#liPrevious").css("background-color","rgb(199, 199, 199)");
+        $("#liPortfolio").css("background-color","rgb(199, 199, 199)");
+        $("#liContact").css("background-color","rgb(199, 199, 199)");
     }
-})
+    
+)
 
-$("#liExperience").on({
-    click: function(){
-        $("#Profile").css("display","none");
-        $("#Experience").css("display","block");
-        $("#Education").css("display","none");
-        $("#Expertise").css("display", "none");
-        $("#Tech").css("display", "none");
-        $("#Goals").css("display", "none");
+$("#liPrevious").click(
+    function(){
+        loadPrevious();
+        $(this).css("background-color","white");
+        $("#liOverview").css("background-color","rgb(199, 199, 199)");
+        $("#liPortfolio").css("background-color","rgb(199, 199, 199)");
+        $("#liContact").css("background-color","rgb(199, 199, 199)");
     }
-})
+)
 
-$("#liEducation").on({
-    click: function(){
-        $("#Profile").css("display","none");
-        $("#Experience").css("display","none");
-        $("#Education").css("display","block");
-        $("#Expertise").css("display", "none");
-        $("#Tech").css("display", "none");
-        $("#Goals").css("display", "none");
+$("#liPortfolio").click(
+    function(){
+        loadPortfolio();
+        $(this).css("background-color","white");
+        $("#liOverview").css("background-color","rgb(199, 199, 199)");
+        $("#liPrevious").css("background-color","rgb(199, 199, 199)");
+        $("#liContact").css("background-color","rgb(199, 199, 199)");
     }
-})
+)
 
-$("#liExpert").on({
-    click: function(){
-        $("#Profile").css("display","none");
-        $("#Experience").css("display","none");
-        $("#Education").css("display","none");
-        $("#Expertise").css("display", "block");
-        $("#Tech").css("display", "none");
-        $("#Goals").css("display", "none");
+$("#liContact").click(
+    function(){
+        loadContact();
+        $(this).css("background-color","white");
+        $("#liOverview").css("background-color","rgb(199, 199, 199)");
+        $("#liPrevious").css("background-color","rgb(199, 199, 199)");
+        $("#liPortfolio").css("background-color","rgb(199, 199, 199)");
     }
-})
+)
+
+let isToggled = false;
+
+let loadOverview = function() {
+    $(document).ready(function() {
+        $("#page_wrapper").load("./overview.html", function() {
+            $("#sub_nav > ul > li").click(
+    
+                function() {
+                    let articleOthers = $("#main_window").children();
+                    let articleObj = $("#main_window").children().eq($(this).index());
+                    let navItems = $(this).parent().children()
+                    navItems.css("background-color", "rgb(216, 216, 216)");
+                    articleOthers.css("display", "none");        
+                    articleObj.css("display", "block");
+                    $(this).css("background-color", "darkorange");
+                }
+            )
+
+            $(".Expdiv0").hover(
+                function() {
+                    if (!isToggled) {
+                    $("#exp_details").html($(this).children().last().html());
+                    $("#exp_details").css("display", "inline");
+                    $("#exp_details").css("top", $(this).parent().position().top+20);
+                    }
+                },
+                function() {
+                    if (!isToggled) {
+                    $("#exp_details").css("display", "none");
+                }
+                }
+            )
+            
+            $(".Expdiv0").click(
+                function() {
+                    isToggled = !isToggled;
+                    let expItems = $(this).parent().parent().children();
+                    $("#exp_details").html($(this).children().last().html());
+                    $("#exp_details").css("display", "inline");
+                    $("#exp_details").css("top", $(this).parent().position().top+20);
+                    if (isToggled) {
+                        $(this).css("color", "darkorange");
+                    }
+                    else {
+                        $(this).css("color", "inherit");
+                        // expItems.css("color", "inherit");
+                    }
+                },
+            )
+        });
+    });
+
+}
+
+let loadPrevious = function() {
+    $(document).ready(function() {
+        $("#page_wrapper").load("./previousWork.html", function()
+        {
+
+        })
+    })
+};
+
+let loadPortfolio = function() {
+    $(document).ready(function() {
+        $("#page_wrapper").load("./portfolio.html", function()
+        {
+
+        })
+    })
+};
+
+
+let loadContact = function() {
+    $(document).ready(function() {
+        $("#page_wrapper").load("./contact.html", function()
+        {
+
+        })
+    })
+};
 
 
 
-$("#liTech").on({
-    click: function(){
-        $("#Profile").css("display","none");
-        $("#Experience").css("display","none");
-        $("#Education").css("display","none");
-        $("#Expertise").css("display", "none");
-        $("#Tech").css("display", "block");
-        $("#Goals").css("display", "none");
-    }
-})
-
-$("#liGoals").on({
-    click: function(){
-        $("#Profile").css("display","none");
-        $("#Experience").css("display","none");
-        $("#Education").css("display","none");
-        $("#Expertise").css("display", "none");
-        $("#Tech").css("display", "none");
-        $("#Goals").css("display", "block");
-    }
-})
 
 
-
-$("#tooltiptest").hover(
-    function() {
-      $( this ).append( $( "<div id='tooled'>I work\
-        at cea</div>" ) );
-    }, function() {
-      $( this ).find( "div" ).last().remove();
-    }
-  );
